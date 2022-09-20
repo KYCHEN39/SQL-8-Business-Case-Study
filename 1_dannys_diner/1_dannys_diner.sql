dbd4a8dbbc91713601341b7cc57d9fb761e56783
kychen39 <rose39iris@gmail.com>
2022-09-19 19:09:57 -0700

    upload revised sql and image for case study 1

--- 1_dannys_diner/1_dannys_diner.sql
+++ 1_dannys_diner/1_dannys_diner.sql
@@ -62,7 +62,7 @@ Order by 1
 
 -- 2. How many days has each customer visited the restaurant?
 -- From 1 and 2, we can see the visiting pattern of each customer. Customer A and B more loyal to Danny's.
-Select customer_id, count(order_date) as total_days_visited
+Select customer_id, count(distinct order_date) as total_days_visited
 From sales
 Group by 1
 Order by 1
@@ -71,14 +71,13 @@ Order by 1
 -- 3. What was the first item from the menu purchased by each customer?
 -- By looking at the first item each customer ordered, we are able to tell which menu item is more attractive for first visiting.
 With cte as (
-Select customer_id, product_name, row_number() over (partition by customer_id order by order_date) as first_item
-From sales As s
-Left Join menu As m on s.product_id = m.product_id
-Order by 1)
+Select customer_id, product_id, dense_rank() over (partition by customer_id order by order_date) as first_item
+From sales)
 
-Select customer_id, product_name
+Select distinct customer_id, product_name
 From cte
-Where first_item = 1
+Join menu As m on cte.product_id = m.product_id
+Where cte.first_item = 1
 Order by 1
 ;
 
@@ -135,3 +134,55 @@ Join menu m on c.product_id = m.product_id
 Where rnk = 1
 Order by 1
 ;
+
+-- 8. What is the total items and amount spent for each member before they became a member?
+With cte as
+(select s.customer_id, product_id, order_date
+From sales s 
+Join members ms on s.customer_id = ms.customer_id
+Where s.order_date < ms.join_date)
+
+Select customer_id, count(product_name) as total_items, sum(price) as total_price
+From cte 
+Join menu m on cte.product_id = m.product_id
+group by cte.customer_id
+order by 1
+;
+
+-- 9. If each $1 spent equates to 10 points and sushi has a 2x points multiplier - how many points would each customer have?
+with cte as
+(Select s.customer_id, s.product_id, m.price, m.product_name,
+(case 
+	when product_name in ('curry','ramen') then price*10
+    else price*20
+end) as points
+From sales s 
+Join menu m on s.product_id = m.product_id)
+
+Select customer_id, sum(points) as total_points
+From cte
+Group by 1
+;
+
+-- 10. In the first week after a customer joins the program (including their join date) they earn 2x points on all items, not just sushi - how many points do customer A and B have at the end of January?
+with cte as
+(Select s.customer_id, order_date, product_id, join_date
+From sales s 
+Join members m on s.customer_id = m.customer_id),
+cte2 as
+(select customer_id, order_date, product_name, price,join_date,
+case 
+	when product_name = 'sushi' then price*20
+	when datediff(order_date,join_date) <= 6 and datediff(order_date,join_date) >= 0 then price*20
+    when datediff(order_date,join_date) >= 7 and product_name in ('curry','ramen') then price*10
+    when datediff(order_date,join_date) < 0 then price*10
+	else price*10
+end as total_points
+from cte
+join menu m on cte.product_id = m.product_id
+order by 1)
+
+select customer_id, sum(total_points)
+from cte2 
+where order_date < '2021-02-01'
+group by 1;
